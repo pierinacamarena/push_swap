7f37e1c7c2c73dcb132677cc46b36014abdefa3d
Pierina <pierinaacam@gmail.com>
2021-12-09 13:42:14 +0100

    fixing bug in second optimization

--- src/reverse/intermediate_bigger_reverse.c
+++ src/reverse/intermediate_bigger_reverse.c
@@ -14,14 +14,16 @@
 
 int	rda_more_midpoint(t_stack *stacka, t_stack *stackb, t_info *printer)
 {
-	printf("inside rda_bigger\n");
 	int	count;
 	int	i;
 	int	new_ra;
+	int	rra;
 
 	i = 0;
 	count = 0;
 	new_ra = 0;
+	rra = 0;
+	printf("---inside rda_more_midpoint---\n instruction number is %d\n", printer->count);
 	while (i < stackb->d_a)
 	{
 		count = count + ft_rra(stacka, stackb, printer);
@@ -29,24 +31,39 @@ int	rda_more_midpoint(t_stack *stacka, t_stack *stackb, t_info *printer)
 	}
 	count = count + ft_pa(stacka, stackb, printer);
 	i = 0;
-	count = count + r_optim_less(stacka, stackb, printer, &new_ra);
-	while (i <= (stackb->d_a + new_ra))
+	while (i <= stackb->d_a)
 	{
+		count = count + r_optim_less(stacka, stackb, printer, &new_ra);
+		rra = rra + new_ra;
+		printf("the next function will be ra inside original loop \n");
+		printf("the instruction before the ra is %d\n", printer->count);
 		count = count + ft_ra(stacka, stackb, printer);
 		i++;
 	}
+	i = 0;
+	while (i < rra)
+	{
+		printf("the next function will be ra of the optimization \n");
+		printf("the instruction before the optimized ra is %d\n", printer->count);
+		ft_ra(stacka, stackb, printer);
+		i++;
+	}
+	printf("---exiting rda_more_midpoint---\n, count is %d\n rra is %d\n", count, rra);
+	printf("the number of instruction is %d\n", printer->count);
 	return (count);
 }
 
 int	rdb_more_midpoint(t_stack *stacka, t_stack *stackb, t_info *printer)
 {
-	printf("inside rdb_bigger\n");
 	int	i;
 	int	count;
 	int	new_ra;
+	int	rra;
 
 	i = 0;
 	new_ra = 0;
+	rra = 0;
+	printf("---inside rdb_more_midpoint---\n instruction number is %d\n", printer->count);
 	count = ft_sb(stackb, stacka, printer);
 	while (i < stackb->d_b)
 	{
@@ -55,23 +72,35 @@ int	rdb_more_midpoint(t_stack *stacka, t_stack *stackb, t_info *printer)
 	}
 	count = count + ft_pa(stacka, stackb, printer);
 	i = 0;
-	count = count + r_optim_less(stacka, stackb, printer, &new_ra);
-	while (i <= (stackb->d_b + new_ra))
+	while (i <= (stackb->d_b))
 	{
 		count = count + ft_ra(stacka, stackb, printer);
+		count = count + r_optim_less(stacka, stackb, printer, &new_ra);
+		rra = rra + new_ra;
 		i++;
 	}
+	i = 0;
+	while (i < rra)
+	{
+		ft_ra(stacka, stackb, printer);
+		i++;
+	}
+	printf("---exiting rdb_more_midpoint---\n, count is %d\n rra is %d\n", count, rra);
+	printf("the number of instruction is %d\n", printer->count);
 	return (count);
 }
 
 int	rdc_more_midpoint(t_stack *stacka, t_stack *stackb, t_info *printer)
 {
-	printf("inside rdc_bigger\n");
 	int	i;
 	int	count;
 	int	new_ra;
+	int	rra;
 
 	i = 0;
+	new_ra = 0;
+	rra = 0;
+	printf("---inside rdc_more_midpoint---\n instruction number is %d\n", printer->count);
 	count = ft_rrb(stackb, stacka, printer);
 	while (i < stackb->d_c)
 	{
@@ -79,12 +108,21 @@ int	rdc_more_midpoint(t_stack *stacka, t_stack *stackb, t_info *printer)
 		i++;
 	}
 	count = count + ft_pa(stacka, stackb, printer);
-	count = count + r_optim_less(stacka, stackb, printer, &new_ra);
 	i = 0;
-	while (i <= (stackb->d_c + new_ra))
+	while (i <= (stackb->d_c))
 	{
+		count = count + r_optim_less(stacka, stackb, printer, &new_ra);
+		rra = rra + new_ra;
 		count = count + ft_ra(stacka, stackb, printer);
 		i++;
 	}
+	i = 0;
+	while (i < rra)
+	{
+		ft_ra(stacka, stackb, printer);
+		i++;
+	}
+	printf("---exiting rdc_more_midpoint---\n, count is %d\n rra is %d\n", count, rra);
+	printf("the number of instruction is %d\n", printer->count);
 	return (count);
 }
