744790f13eb194881d089d83454c2e6174eda35c
Pierina alessandra CAMARENA PUCUHUANCA <pcamaren@e3r5p21.clusters.42paris.fr>
2022-01-05 18:10:41 +0100

    working on the push_to_be_part

--- src/push_to_b/push_to_b.c
+++ src/push_to_b/push_to_b.c
@@ -11,30 +11,57 @@
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
+/*
+
+Part II: From Stack A to Stack B
+Pseudocode
+
+WHILE stack A has elements with "false" value in "Keep in Stack A" field
+      IF sa (swap a) is needed
+            perform sa (swap a) command
+            update markup
+      ELSE IF head element of stack A has "false" value in "Keep in Stack A" field
+            perform pb (push b) command
+      ELSE
+            perform ra (rotate a) command
+How to check that sa (swap a) is needed?
+You have to perform sa (swap a) and then remake markup. We only have to update markup with chosen at previous steps parameters (as markup_head).
+
+Then we have to compare how many elements will be kept in stack A with performed sa (swap a) and without it.
+
+If after performing sa (swap a) more elements will be kept, it means that there is a reason to do it.
+
+After all we have to unmake sa (swap a) and return result of our calculations.
+*/
 
 void    push_to_b(t_stack *stacka);
 {
     int i;
-    int bool_check;
+    int stacka_check;
     int count;
 
     i = 0;
     count = 0;
-    bool_check = check_bool_array(stacka);
-    while (bool_check == 1)
+    stacka_check = check_bool_array(*stacka);
+    while (stacka_check == 1)
     {
-        if (stacka->bool_index[0] == 0)
-            count = ft_pb(stacka, stackb, printer);   
+        stacka_check = check_bool_array(*stacka);
+        //check if sa is needed
+        else if (stacka->array[0] == 0)
+            count = count + ft_pb(stacka, stackb, printer);
+        else
+            count = count + ft_ra(stacka, stackb, printer);   
     }
 }
 
-int check_bool_array(t_stack stacka)
+int check_stacka(t_stack stacka)
 {
     int i;
+
     i = 0;
-    while(i < stacka.size)
+    while (i < stacka.size)
     {
-        if (stacka.bool_index[i] == 1)
+        if (stacka->array[i][2] == 0)
             return (1);
         i++;
     }
